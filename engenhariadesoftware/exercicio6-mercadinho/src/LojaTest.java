80b285a05a3b98d8f7a6504e543aca5aed682b2d
Vincius <viniviniee11@gmail.com>
2026-03-06 19:26:23 -0300

    test: adiciona teste para os métodos removerProduto(), lerTamanhoLista()

--- engenhariadesoftware/exercicio6-mercadinho/src/LojaTest.java
+++ engenhariadesoftware/exercicio6-mercadinho/src/LojaTest.java
@@ -14,4 +14,28 @@ class LojaTest {
         assertEquals(1,loja.getProdutos().size());
 
     }
+    @Test
+    void removerProduto() {
+
+        Loja loja = new Loja();
+
+        Produto produto = new Produto("Computador Dell", 3999.90);
+        loja.adicionarProduto(produto);
+
+        loja.removerProduto(0);
+
+        assertEquals(0,loja.getProdutos().size());
+    }
+    @Test
+    void lerTamanhoLista(){
+        Loja loja = new Loja();
+
+        Produto produto1 = new Produto("Computador Dell", 3999.90);
+        Produto produto2 = new Produto("Suco Maguary Morango 300ml", 9.90);
+        loja.adicionarProduto(produto1);
+        loja.adicionarProduto(produto2);
+
+        assertEquals(2,loja.lerTamanhoLista());
+
+    }
 }
