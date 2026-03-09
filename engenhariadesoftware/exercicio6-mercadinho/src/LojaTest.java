19cafa589c7bc58f22ce84c6ee451a36965b7ad5
Vincius <viniviniee11@gmail.com>
2026-03-09 20:49:10 -0300

    test: adiciona o teste do método de encontrar produto por nome e preço

--- engenhariadesoftware/exercicio6-mercadinho/src/LojaTest.java
+++ engenhariadesoftware/exercicio6-mercadinho/src/LojaTest.java
@@ -38,4 +38,32 @@ class LojaTest {
         assertEquals(2,loja.lerQuantidadeProdutos());
 
     }
+
+    @Test
+    void encontrarPorNomePreco() {
+
+        Loja loja = new Loja();
+
+        Produto produto1 = new Produto("Computador Dell", 3999.90);
+        Produto produto2 = new Produto("Suco Maguary Morango 300ml", 9.90);
+        Produto produto1copia1 = new Produto("Computador Dell", 3999.90);
+
+        List<Produto> produtosEncontrados = new LinkedList<>();
+
+        loja.adicionarProduto(produto1);
+        loja.adicionarProduto(produto1copia1);
+        loja.adicionarProduto(produto2);
+
+        Produto produto1copia2 = new Produto("Computador Dell", 3999.90);
+        Produto produto1copia3 = new Produto("Computador Dell", 3999.90);
+        Produto produto1copia4 = new Produto("Computador Dell", 3999.90);
+
+        loja.adicionarProduto(produto1copia2);
+        loja.adicionarProduto(produto1copia3);
+        loja.adicionarProduto(produto1copia4);
+
+        produtosEncontrados = loja.encontrarPorNomePreco(produto1);
+        assertEquals(5, produtosEncontrados.size());
+
+    }
 }
